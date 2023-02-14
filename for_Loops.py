b392bfdaac704f417563e691ea286e88f1830285
mdfaisalmahamud <mdfaisalmollik7@gmail.com>
2023-02-14 23:45:32 +0600

    Python-for-Loops

---
+++ for_Loops.py
@@ -0,0 +1,73 @@
+# for x in sequence:
+#     statement1
+#     statement2
+#     ...
+#     statement
+
+nums = [10, 20, 30, 40, 50]
+
+for i in nums:
+    print(i)
+
+
+
+    # Example: For Loop with Tuple
+
+    nums = (10, 20, 30, 40, 50)
+for i in nums:
+    print(i)
+
+    # Example: For Loop with String
+
+    for char in 'Hello':
+    print (char)
+
+    # Example: For Loop with Dictionary
+
+    numNames = { 1:'One', 2: 'Two', 3: 'Three'}
+
+for pair in numNames.items():
+    print(pair)
+
+
+    # Example: For Loop with Dictionary
+    numNames = { 1:'One', 2: 'Two', 3: 'Three'}
+
+for k,v in numNames.items():
+    print("key = ", k , ", value =", v)
+
+    # For Loop with the range() Function
+
+    for i in range(5):
+    print(i)
+
+
+    # Exit the For Loop
+
+    for i in range(1, 5):
+    if i > 2
+        break
+    print(i)
+
+    # Continue Next Iteration
+
+    for i in range(1, 5):
+    if i > 3
+        continue
+    print(i)
+
+    # For Loop with Else Block
+
+    for i in range(2):
+    print(i)
+else:
+     print('End of for loop')
+
+    #  Nested for Loop
+    for x in range(1,4):
+    for y in range(1,3):
+        print('x = ', x, ', y = ', y) 
+
+        
+
+
